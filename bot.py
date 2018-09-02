c1e224864d7d015d91821189b7a7c044d60cd280
thewoodenarcher <41194285+thewoodenarcher@users.noreply.github.com>
2018-09-02 14:33:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def _from(ctx, user: discord.Member, *, command: str):
     ctx.message.author = user
     ctx.message.content = f"{ctx.prefix}{command}"
     await bot.process_commands(ctx.message)
-@bot.command(
+@bot.command()
 async def servername(ctx):
     await ctx.send (guild.name)
     
