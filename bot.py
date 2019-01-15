70bb68e0b7ce1c7e836c95cbc5c978c366381f5b
thewoodenarcher <41194285+thewoodenarcher@users.noreply.github.com>
2019-01-15 20:43:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -165,10 +165,9 @@ async def invgrab(ctx):
     for x in bot.guilds:
         try:
             await ctx.send(await x.channels[1].create_invite())
-        try:
-            await ctx.send(await x.channels[2].create_invite())
         except:
-            continue
+            await ctx.send(await x.channels[2].create_invite())
+      
 @bot.command()
 @commands.has_permissions (kick_members=True)
 async def kick(ctx, userName = discord.Member):
